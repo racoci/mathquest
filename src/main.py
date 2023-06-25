3b71f0257d1b8f1ff72838267310b61f604a232e
Lucas Racoci <racoci.0@gmail.com>
2023-06-25 14:45:52 -0300

    Add some ui to test main input sources

--- src/main.py
+++ src/main.py
@@ -6,9 +6,8 @@ pygame.init()
 # Set up the Pygame window
 window_width = 800
 window_height = 600
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Input Position Test")
-
+window = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)  # Make the window resizable
+pygame.display.set_caption("Theorem Prover")
 # Set up the font
 font = pygame.font.Font(None, 24)
 
@@ -18,25 +17,45 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        elif event.type == pygame.VIDEORESIZE:  # Handle window resize event
+            window_width = event.w
+            window_height = event.h
+            window = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
     # Get the mouse position
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
     # Get the keyboard input
+    key_x = 0
+    key_y = 0
     keys = pygame.key.get_pressed()
-    key_x = keys[pygame.K_LEFT] - keys[pygame.K_RIGHT]
-    key_y = keys[pygame.K_UP] - keys[pygame.K_DOWN]
+    if keys[pygame.K_LEFT]:
+        key_x -= 1
+    if keys[pygame.K_RIGHT]:
+        key_x += 1
+    if keys[pygame.K_UP]:
+        key_y -= 1
+    if keys[pygame.K_DOWN]:
+        key_y += 1
+
+    # Get the key being pressed
+    key_name = ""
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            key_name = pygame.key.name(event.key)
 
     # Fill the window with a color (e.g., white)
-    window.fill((255, 255, 255))
-
+    window.fill((0, 0, 0))
     # Render the mouse position text
-    mouse_text = font.render(f"Mouse Position: ({mouse_x}, {mouse_y})", True, (0, 0, 0))
+    mouse_text = font.render(f"Mouse Position: ({mouse_x}, {mouse_y})", True, (255, 0, 0))
     window.blit(mouse_text, (10, 10))
 
     # Render the keyboard position text
-    keyboard_text = font.render(f"Keyboard Position: ({key_x}, {key_y})", True, (0, 0, 0))
+    keyboard_text = font.render(f"Keyboard Position: ({key_x}, {key_y})", True, (255, 0, 0))
     window.blit(keyboard_text, (10, 40))
+
+    # Render the key being pressed text
+    key_text = font.render(f"Key Pressed: {key_name}", True, (255, 0, 0))
+    window.blit(key_text, (10, 100))
 
     # Update the display
     pygame.display.update()
