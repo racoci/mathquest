52d8a4d029f333e3c471c71dc759ff1f259dac82
Lucas Racoci <racoci.0@gmail.com>
2023-06-25 14:26:42 -0300

    Modify main.py to get mouse position and show it on screen.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,10 @@ pygame.init()
 window_width = 800
 window_height = 600
 window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Pygame Test")
+pygame.display.set_caption("Mouse Position Test")
+
+# Set up the font
+font = pygame.font.Font(None, 24)
 
 # Game loop
 running = True
@@ -16,8 +19,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Get the mouse position
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+
     # Fill the window with a color (e.g., white)
     window.fill((255, 255, 255))
+
+    # Render the mouse position text
+    text = font.render(f"Mouse Position: ({mouse_x}, {mouse_y})", True, (0, 0, 0))
+    window.blit(text, (10, 10))
 
     # Update the display
     pygame.display.update()
